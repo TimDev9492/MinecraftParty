ddbdc4ef4b4648deb25c813353cdfefd20d7eca6
tim <tim@timdev.de>
2022-03-04 21:37:45 +0100

    added build height to duels

--- src/main/java/me/timwastaken/minecraftparty/listeners/GameListener.java
+++ src/main/java/me/timwastaken/minecraftparty/listeners/GameListener.java
@@ -60,6 +60,10 @@ public class GameListener implements Listener {
             if (event.getBlockPlaced().getLocation().getBlockY() > mlgRushMinigame.getBuildHeight() || mlgRushMinigame.isNearOwnBed(event.getPlayer(), event.getBlock())) {
                 event.setCancelled(true);
             }
+        } else if (GameManager.getActiveMinigame() instanceof Duels duelsMinigame) {
+            if (event.getBlockPlaced().getLocation().getBlockY() > duelsMinigame.getBuildHeight()) {
+                event.setCancelled(true);
+            }
         }
         if (!event.isCancelled()) GameManager.getActiveMinigame().addPlacedBlock(event.getBlockPlaced());
     }
